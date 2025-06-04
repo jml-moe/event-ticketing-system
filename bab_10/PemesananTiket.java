d1fd2284d2a3dff3a38bb2ce53f5b0a86c4b6bfb
jml-moe <2318069@student.itn.ac.id>
2025-06-04 10:25:16 +0700

    tugas

---
+++ bab_10/PemesananTiket.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bab_10;
+
+import bab_7.*;
+
+
+/**
+ *
+ * @author jamil
+ */
+
+public class PemesananTiket extends EventTicket {
+     // Constructor
+    public PemesananTiket(String namaEvent, String lokasi, String tanggal, double harga, int jumlahTiket) {
+        super(namaEvent, lokasi, tanggal, harga, jumlahTiket);
+    }
+
+    // Override cekStatusTiket
+    @Override
+    public String cekStatusTiket() {
+        return getJumlahTiket() > 0 ? "Tiket tersedia." : "Tiket habis.";
+    }
+}   
+
+
+
+
